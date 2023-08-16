3416c91b9bc0b125c2a9f802ceda0335cfd01e53
Vivian Kuo <vivian.kuo4@outlook.com>
2023-08-15 16:07:09 -1000

    added retrieveRole.js

---
+++ retrieveRole.js
@@ -0,0 +1,20 @@
+const db = require('../config/connection.js');
+
+async function retrieveRole(response) {
+    let roleList;
+    let returnedValue = await db.promise().query(`
+    SELECT * FROM role
+    `).then( (data) => {
+        roleList = data[0].map(data => {
+            return {
+                name: data.title,
+                value: data.id
+            }
+        })
+    }).catch(err=>{
+        console.log(err);
+    })
+    return returnedValue
+};
+
+module.exports = retrieveRole;

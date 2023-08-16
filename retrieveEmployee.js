9de28dc09d692dfebb6d80f15a5620192cc6913c
Vivian Kuo <vivian.kuo4@outlook.com>
2023-08-15 15:29:43 -1000

    retrieveEmployee.js initial commit

---
+++ retrieveEmployee.js
@@ -0,0 +1,22 @@
+const db = require('../config/connection.js');
+
+async function retrieveEmployee(response) {
+    let employeeList;
+    let returnedValue = await db.promise().query(`
+    SELECT * FROM employee
+    `).then( (data) => {
+        employeeList = data[0].map(data => {
+            return {
+                name: data.first_name + ` ` + data.last_name,
+                value: data.id
+            }
+        })
+        employeeList.push({name: 'NONE', value:0});
+        return employeeList
+    }).catch(err=>{
+        console.log(err);
+    })
+    return returnedValue
+};
+
+module.exports = retrieveEmployee;

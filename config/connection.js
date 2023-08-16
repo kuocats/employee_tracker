1bf03dbcfba67af50cc93ffa1a843eeb7c395e81
Vivian Kuo <vivian.kuo4@outlook.com>
2023-08-15 17:00:13 -1000

    rearranged files, added connection.js file

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        port: '3306',
+        user:'root',
+        password: process.env.PASSWORD,
+        database: 'employee_db'
+    },
+
+   console.log('\u001b[33m', `Connected to the employee_db database!`) 
+);
+
+module.exports = db;
